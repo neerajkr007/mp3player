7333edfda8fa23993e4aaba918d1cdc9926a7f68
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-06-27 16:48:10 +0530

    added functionality play/unpause, pause, stop

--- test/gui.py
+++ test/gui.py
@@ -1,13 +1,70 @@
 import tkinter as tk
+from tkinter import filedialog
+import tkinter.messagebox
+from pygame import mixer
 
 root = tk.Tk()
+mixer.init()
+
 canvas = tk.Canvas(root, height=300, width=300)
 canvas.pack()
 
 frame = tk.Frame(root, bg='#80c1ff')
-frame.place(relx=0.1, rely=0.1, relheight=0.5, relwidth=0.5)
+frame.place(relheight=1, relwidth=1)
 
-button = tk.Button(root, text='Test button', fg='blue')
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+submenu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label='File', menu=submenu)
+
+
+def file_browser():
+    global filename
+    filename = filedialog.askopenfilename()
+    print(filename)
+
+
+submenu.add_command(label='open', command=file_browser)
+
+
+def load_play():
+    if mixer.music.get_pos() == -1:
+        try:
+            mixer.music.load(filename)
+            mixer.music.play(0)
+        except:
+            tkinter.messagebox.showinfo(title='Error', message="Load a file first")
+    else:
+        mixer.music.unpause()
+
+
+button = tk.Button(root, text='play', fg='blue', command=load_play)
 button.pack()
+
+
+def pause():
+    mixer.music.pause()
+
+
+button = tk.Button(root, text='pause', fg='blue', command=pause)
+button.pack()
+
+
+def stop():
+    mixer.music.stop()
+
+
+button = tk.Button(root, text='stop', fg='blue', command=stop)
+button.pack()
+
+
+def pos():
+    print(mixer.music.get_pos())
+
+
+button = tk.Button(root, text='pos', fg='blue', command=pos)
+button.pack()
+# message = tk.Message(frame, text='test_player')
+# message.pack()
 
 root.mainloop()
